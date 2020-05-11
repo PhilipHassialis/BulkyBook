396da5c6b065c273cf9bf43fea8da8238bdb79b3
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-05-11 07:18:10 +0300

    Company and Application User models

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -15,6 +15,9 @@ namespace BulkyBook.Models
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
 
         [NotMapped]
         public string Role { get; set; }
