18403f16e1b01361e4f4875902c681b21283f340
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-05-18 07:09:22 +0300

    Send Email

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,22 +11,25 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        private readonly EmailOptions emailOptions;
+
+        public EmailSender(IOptions<EmailOptions> options)
         {
-            throw new NotImplementedException();
+            emailOptions = options.Value;
         }
 
-        static async Task Execute()
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            return Execute(emailOptions.SendGridKey, subject, htmlMessage, email);
+        }
+
+        private Task Execute(string sendGrigKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGrigKey);
+            var from = new EmailAddress("admin@bulky.com", "Bulky Books");
+            var to = new EmailAddress(email, "End User");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "",message);
+            return client.SendEmailAsync(msg);
         }
     }
 }
