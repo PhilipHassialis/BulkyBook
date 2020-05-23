1edb42b833d1ec9e61af1ff2e0e0c9acfbdebcad
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-05-23 18:07:31 +0300

    Add Repository for Models

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -20,6 +20,10 @@ namespace BulkyBook.DataAccess.Repository
             ApplicationUser = new ApplicationUserRepository(_db);
                  
             SP_Call = new SP_Call(_db);
+
+            ShoppingCart = new ShoppingCartRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetails = new OrderDetailsRepository(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
@@ -27,6 +31,9 @@ namespace BulkyBook.DataAccess.Repository
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IOrderHeaderRepository OrderHeader{ get; private set; }
+        public IOrderDetailsRepository OrderDetails { get; private set; }
         public ISP_Call SP_Call { get; private set; }
 
         public void Dispose()
