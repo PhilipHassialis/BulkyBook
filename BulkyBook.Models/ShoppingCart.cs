23801f06b71010da1564edccbfa296aac30ba4fa
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-05-24 15:33:03 +0300

    Product Details

--- BulkyBook.Models/ShoppingCart.cs
+++ BulkyBook.Models/ShoppingCart.cs
@@ -20,7 +20,7 @@ namespace BulkyBook.Models
 
         public int ProductId { get; set; }
         [ForeignKey("ProductId")]
-        Product Product { get; set; }
+        public Product Product { get; set; }
         [Range(1,1000,ErrorMessage ="Please enter a value between 1 and 1000")]
         public int Count { get; set; }
         [NotMapped]
