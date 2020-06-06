b88d2e1263423988058e9165cf542727cc63ddac
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-06-06 12:20:44 +0300

    Adding and Configuring Stripe

---
+++ BulkyBook.Utility/StripeSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Utility
+{
+    public class StripeSettings
+    {
+        string PublishableKey { get; set; }
+        string SecretKey { get; set; }
+    }
+}
--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -16,6 +16,7 @@ using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.DataAccess.Repository;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
+using Stripe;
 
 namespace BulkyBook
 {
@@ -39,6 +40,7 @@ namespace BulkyBook
             services.AddSingleton<IEmailSender, EmailSender>();
 
             services.Configure<EmailOptions>(Configuration);
+            services.Configure<StripeSettings>(Configuration.GetSection("Stripe"));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
@@ -86,6 +88,8 @@ namespace BulkyBook
             app.UseStaticFiles();
 
             app.UseRouting();
+            StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["SecretKey"];
+
             app.UseSession();
             app.UseAuthentication();
             app.UseAuthorization();
